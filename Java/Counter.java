00cb2f85d9509f793cdacfaec8e9183d57ce1b6f
gy-mag <snagi.sh1@gmail.com>
2025-05-12 16:08:22 +0900

    fix: Counter

--- Java/Counter.java
+++ Java/Counter.java
@@ -1,6 +1,6 @@
 public class Counter {
     public void run() {
-        for (int i = 1; i <= 15; i++) {
+        for (int i = 1; i <= 28; i++) {
             if (new FizzBuzzChecker().isFizzBuzz(i)) {
                 System.out.println("fizzbuzz");
             } else if (new FizzChecker().isFizz(i)) {
